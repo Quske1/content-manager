19118bfb26d93c802c76a4f36fddef5accb5be09
Quske1 <57774912+Quske1@users.noreply.github.com>
2021-05-02 10:48:18 +0300

    Добавьте файлы проекта.

---
+++ ManagerFree/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ManagerFree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ishodopis = new System.Windows.Forms.TextBox();
+            this.resultbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.Marketbox = new System.Windows.Forms.CheckBox();
+            this.Sitebox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ishodopis
+            // 
+            this.ishodopis.Location = new System.Drawing.Point(12, 266);
+            this.ishodopis.Multiline = true;
+            this.ishodopis.Name = "ishodopis";
+            this.ishodopis.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.ishodopis.Size = new System.Drawing.Size(967, 96);
+            this.ishodopis.TabIndex = 3;
+            this.ishodopis.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // resultbox
+            // 
+            this.resultbox.Location = new System.Drawing.Point(12, 396);
+            this.resultbox.Multiline = true;
+            this.resultbox.Name = "resultbox";
+            this.resultbox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.resultbox.Size = new System.Drawing.Size(967, 93);
+            this.resultbox.TabIndex = 4;
+            this.resultbox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(765, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Кол-во символов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(891, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(429, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Исходное описание";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(471, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Результат";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(862, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 18);
+            this.label5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(768, 370);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 18);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Кол-во символов:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(894, 370);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 18);
+            this.label7.TabIndex = 11;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1028, 217);
+            this.webBrowser1.TabIndex = 12;
+            this.webBrowser1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(658, 238);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 22);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Отобразить";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(661, 368);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(109, 22);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "Отобразить";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Marketbox
+            // 
+            this.Marketbox.AutoSize = true;
+            this.Marketbox.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Marketbox.Location = new System.Drawing.Point(250, 235);
+            this.Marketbox.Name = "Marketbox";
+            this.Marketbox.Size = new System.Drawing.Size(93, 27);
+            this.Marketbox.TabIndex = 15;
+            this.Marketbox.Text = "Маркет";
+            this.Marketbox.UseVisualStyleBackColor = true;
+            this.Marketbox.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Sitebox
+            // 
+            this.Sitebox.AutoSize = true;
+            this.Sitebox.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Sitebox.Location = new System.Drawing.Point(83, 238);
+            this.Sitebox.Name = "Sitebox";
+            this.Sitebox.Size = new System.Drawing.Size(118, 27);
+            this.Sitebox.TabIndex = 16;
+            this.Sitebox.Text = "Для сайта";
+            this.Sitebox.UseVisualStyleBackColor = true;
+            this.Sitebox.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1006, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 265);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Отчистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 505);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Sitebox);
+            this.Controls.Add(this.Marketbox);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultbox);
+            this.Controls.Add(this.ishodopis);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Контент-менеджер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox ishodopis;
+        private System.Windows.Forms.TextBox resultbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox Marketbox;
+        private System.Windows.Forms.CheckBox Sitebox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ManagerFree/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using HtmlDocument = System.Windows.Forms.HtmlDocument;
+
+namespace ManagerFree
+{
+    public partial class Form1 : Form
+    {
+        public string zadalovokn = "<h2";
+        public string zadalovokk = "</h2>";
+        public string zadalovoknnew = "<h3";
+        public string zadalovokknnew = "</h3>";
+        public string oln = "<ol>";
+        public string olnk = "</ol>";
+        public string uln = "<ul>";
+        public string ulnk = "</ul>";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private static void SetWebBrowserCompatiblityLevel()
+        {
+            string appName = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+            int lvl = 1000 * GetBrowserVersion();
+            bool fixVShost = File.Exists(Path.ChangeExtension(Application.ExecutablePath, ".vshost.exe"));
+
+            WriteCompatiblityLevel("HKEY_LOCAL_MACHINE", appName + ".exe", lvl);
+            if (fixVShost) WriteCompatiblityLevel("HKEY_LOCAL_MACHINE", appName + ".vshost.exe", lvl);
+
+            WriteCompatiblityLevel("HKEY_CURRENT_USER", appName + ".exe", lvl);
+            if (fixVShost) WriteCompatiblityLevel("HKEY_CURRENT_USER", appName + ".vshost.exe", lvl);
+        }
+
+        private static void WriteCompatiblityLevel(string root, string appName, int lvl)
+        {
+            try
+            {
+                Microsoft.Win32.Registry.SetValue(root + @"\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", appName, lvl);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static int GetBrowserVersion()
+        {
+            string strKeyPath = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Internet Explorer";
+            string[] ls = new string[] { "svcVersion", "svcUpdateVersion", "Version", "W2kVersion" };
+
+            int maxVer = 0;
+            for (int i = 0; i < ls.Length; ++i)
+            {
+                object objVal = Microsoft.Win32.Registry.GetValue(strKeyPath, ls[i], "0");
+                string strVal = Convert.ToString(objVal);
+                if (strVal != null)
+                {
+                    int iPos = strVal.IndexOf('.');
+                    if (iPos > 0)
+                        strVal = strVal.Substring(0, iPos);
+
+                    int res = 0;
+                    if (int.TryParse(strVal, out res))
+                        maxVer = Math.Max(maxVer, res);
+                }
+            }
+
+            return maxVer;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Text = ishodopis.Text.Length.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            label7.Text = resultbox.Text.Length.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+
+            if (checkBox2.Checked == false)
+            {
+                checkBox1.Enabled = true;
+                webBrowser1.Visible = false;
+                HtmlDocument doc = webBrowser1.Document;
+                doc.Body.InnerHtml = null;
+                System.Windows.Forms.Application.DoEvents();
+
+            }
+            else
+            {
+                    checkBox1.Enabled = false;
+                    webBrowser1.Visible = true;
+                    HtmlDocument doc = webBrowser1.Document;
+                    doc.Body.InnerHtml = resultbox.Text;
+                
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+            {
+                checkBox2.Enabled = true;
+                webBrowser1.Visible = false;
+                HtmlDocument doc = webBrowser1.Document;
+                doc.Body.InnerHtml = null;
+                System.Windows.Forms.Application.DoEvents();
+            }
+            else
+            {
+                checkBox2.Enabled = false;
+                        webBrowser1.Visible = true;
+                    HtmlDocument doc = webBrowser1.Document;
+                    doc.Body.InnerHtml = ishodopis.Text;
+                
+
+            }
+
+
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+          if(Marketbox.Checked==true)
+            { Sitebox.Enabled = false;
+              
+                    if (ishodopis.Text == "")
+                    {
+                        Marketbox.Checked = false;
+                        MessageBox.Show(
+                "Поле не содержит кода",
+                "Ошибка",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+                        return;
+                    }
+                    else
+                    {
+
+
+                        string isxod = ishodopis.Text;
+                        isxod = Regex.Replace(isxod, "\\s+", " ");
+                        isxod = isxod.Replace(zadalovokn, zadalovoknnew);
+                        isxod = isxod.Replace(zadalovokk, zadalovokknnew);
+                        isxod = isxod.Replace(oln, uln);
+                        isxod = isxod.Replace(olnk, ulnk);
+                        isxod = isxod.Replace("<tr>", "<li>");
+                        isxod = isxod.Replace("<tbody>", "<ul>");
+                        isxod = isxod.Replace("</tbody>", "</ul>");
+                        isxod = isxod.Replace("<li><li>", "<li>");
+                        isxod = isxod.Replace("<br> <br>", "<br>"); 
+                        isxod = isxod.Replace("<li>  ", " ");
+                        isxod = Regex.Replace(isxod, "<div[^>]+>", string.Empty);
+                        isxod = Regex.Replace(isxod, "</div>", string.Empty);
+                      
+                        isxod = Regex.Replace(isxod, "</a>", string.Empty);
+                        isxod = Regex.Replace(isxod, "<a[^>]+>", string.Empty);
+                        isxod = Regex.Replace(isxod, "<blockquote[^>]+>", string.Empty);
+                        isxod = Regex.Replace(isxod, "</blockquote>", string.Empty);
+                        isxod = Regex.Replace(isxod, "<strong>", string.Empty);
+                        isxod = Regex.Replace(isxod, "</strong>", string.Empty);
+                        isxod = Regex.Replace(isxod, "align=[^>]+", string.Empty);
+                        isxod = Regex.Replace(isxod, "<table class=[^>]+>", string.Empty);
+                        isxod = Regex.Replace(isxod, "</table>", string.Empty);
+                        isxod = Regex.Replace(isxod, "<td>", string.Empty);
+                        isxod = Regex.Replace(isxod, "</td>", string.Empty);
+                        isxod = Regex.Replace(isxod, "</tr>", string.Empty);
+                        isxod = Regex.Replace(isxod, "<img[^>]+>", string.Empty);
+                        isxod = Regex.Replace(isxod, "<thead>(.*?)</thead>", "", RegexOptions.IgnoreCase);
+                        isxod = Regex.Replace(isxod, "<p[^>]+></p>", string.Empty);
+                        isxod = isxod.Replace("<li>  <ul>", "<ul>");
+                        isxod = Regex.Replace(isxod, "<th[^>]+>", string.Empty);
+                        isxod = Regex.Replace(isxod, "</th>", string.Empty);
+                        string outText = isxod
+                                  .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                                  .Where(str => !string.IsNullOrWhiteSpace(str))
+                                  .Aggregate("", (rez, str) => rez + str.Trim() + "\r\n");
+
+                        resultbox.Text = outText;
+
+                    }
+                
+               
+
+            }
+            else
+            {
+                resultbox.Text = null;
+                Sitebox.Enabled = true;
+            }
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Sitebox.Checked == true)
+            {
+                Marketbox.Enabled = false;
+
+                if (ishodopis.Text == "")
+                {
+                    Sitebox.Checked = false;
+                    MessageBox.Show(
+            "Поле не содержит кода",
+            "Ошибка",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error,
+            MessageBoxDefaultButton.Button1,
+            MessageBoxOptions.DefaultDesktopOnly);
+                    return;
+                }
+                else
+                {
+                    string isxod = ishodopis.Text;
+                    isxod = Regex.Replace(isxod, "<table[^>]+>", "<table class=\"colored_table\">");
+                    isxod = Regex.Replace(isxod, "<div[^>]+>", string.Empty);
+                    isxod = Regex.Replace(isxod, "<tr[^>]+>", "<tr>");
+                    isxod = Regex.Replace(isxod, "<th[^>]+>", "<th>");
+                    isxod = Regex.Replace(isxod, "<td[^>]+>", "<td>");
+                    isxod = Regex.Replace(isxod, "<ul[^>]+>", "<ul>");
+                    isxod = Regex.Replace(isxod, "<li[^>]+>", "<li>");
+                    isxod = Regex.Replace(isxod, "<ol[^>]+>", "<ol>");
+                    isxod = Regex.Replace(isxod, "<div[^>]+>", string.Empty);
+                    isxod = Regex.Replace(isxod, "</div>", string.Empty);
+                    isxod = Regex.Replace(isxod, "</a>", string.Empty);
+                    isxod = Regex.Replace(isxod, "<a[^>]+>", string.Empty);
+                    isxod = Regex.Replace(isxod, "<blockquote[^>]+>", string.Empty);
+                    isxod = Regex.Replace(isxod, "</blockquote>", string.Empty);
+                    isxod = Regex.Replace(isxod, "<strong>", string.Empty);
+                    isxod = Regex.Replace(isxod, "</strong>", string.Empty);
+                    isxod = Regex.Replace(isxod, "align=[^>]+", string.Empty);
+                    isxod = Regex.Replace(isxod, "<img[^>]+>", string.Empty);
+                    isxod = Regex.Replace(isxod, "<p[^>]+></p>", string.Empty);
+                    isxod = isxod.Replace("<li>  <ul>", "<ul>");
+                    string outText = isxod
+                             .Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                              .Where(str => !string.IsNullOrWhiteSpace(str))
+                            .Aggregate("", (rez, str) => rez + str.Trim() + "\r\n");
+
+                    resultbox.Text = outText;
+
+                }
+            }
+            else
+            {
+                resultbox.Text = null;
+                Marketbox.Enabled = true;
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            resultbox.Text = null;
+            ishodopis.Text = null;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            Marketbox.Checked = false;
+            Sitebox.Checked = false;
+            checkBox1.Enabled = true;
+            checkBox2.Enabled = true;
+            Marketbox.Enabled = true;
+            Sitebox.Enabled = true;
+            webBrowser1.Visible = false;
+            HtmlDocument doc = webBrowser1.Document;
+            doc.Body.InnerHtml = null;
+            System.Windows.Forms.Application.DoEvents();
+        }
+    }
+    
+}
